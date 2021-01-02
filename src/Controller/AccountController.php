9adfb8bb038800bcf8c3ecf318517ea92a09ad3f
gopibabu <s.gopibabu@gmail.com>
2021-01-01 20:33:14 -0600

    📦 NEW: accessing user object

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,20 +3,20 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @IsGranted("ROLE_USER")
  */
-class AccountController extends AbstractController
+class AccountController extends BaseController
 {
     /**
      * @Route("/account", name="app_account")
      */
     public function index(): Response
     {
+        $this->getUser()->getEmail();
         return $this->render('account/index.html.twig', [
 
         ]);
