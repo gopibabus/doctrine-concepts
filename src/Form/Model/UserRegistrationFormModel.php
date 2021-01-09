a705545acd2c813fc2a8651536e12af7595a3c0f
gopibabu <s.gopibabu@gmail.com>
2021-01-09 17:05:33 -0600

    📦 NEW: added customvalidator

--- src/Form/Model/UserRegistrationFormModel.php
+++ src/Form/Model/UserRegistrationFormModel.php
@@ -2,17 +2,15 @@
 
 namespace App\Form\Model;
 
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use App\Validator\UniqueUser;
 use Symfony\Component\Validator\Constraints as Assert;
 
-/**
- * @UniqueEntity(fields={"email"}, message="This email is already registered")
- */
 class UserRegistrationFormModel
 {
     /**
      * @Assert\NotBlank(message="Please eneter an email")
      * @Assert\Email()
+     * @UniqueUser()
      */
     public $email;
 
