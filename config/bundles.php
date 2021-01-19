508762f802eea600db9e85de42573454a4ffb5d9
gopibabu <s.gopibabu@gmail.com>
2021-01-19 13:46:35 -0600

    📦 NEW: converted internal styles into inline styles

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
