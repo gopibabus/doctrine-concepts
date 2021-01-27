348b084c26b8770385fa81b2a29f07c0a2535244
gopibabu <s.gopibabu@gmail.com>
2021-01-26 20:45:17 -0600

    📦 NEW: added integration test case to authorWeeklyReport

--- tests/Service/MailerTest.php
+++ tests/Service/MailerTest.php
@@ -2,15 +2,16 @@
 
 namespace App\Tests\Service;
 
+use App\Entity\Article;
 use App\Entity\User;
 use App\Service\Mailer;
 use Knp\Snappy\Pdf;
-use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 use Twig\Environment;
 
-class MailerTest extends TestCase
+class MailerTest extends kernelTestCase
 {
     public function testSendWelcomeMessage()
     {
@@ -34,5 +35,27 @@ class MailerTest extends TestCase
         $this->assertInstanceOf(Address::class, $address[0]);
         $this->assertSame('Wonderful User', $address[0]->getName());
         $this->assertSame('gopi@gmail.com', $address[0]->getAddress());
+    }
+
+    public function testIntegrationSendAuthorWeeklyReportMessage()
+    {
+        self::bootKernel();
+
+        $symfonyMailer = $this->createMock(MailerInterface::class);
+        $symfonyMailer->expects($this->once())->method('send');
+
+        $pdf = self::$container->get(Pdf::class);
+        $twig = self::$container->get(Environment::class);
+
+        $user = new User();
+        $user->setFirstName('Gopi');
+        $user->setEmail('gopi@gmail.com');
+        $article = new Article();
+        $article->setTitle('Black Hole: Real');
+
+        $mailer = new Mailer($symfonyMailer, $twig, $pdf);
+        $email = $mailer->sendAuthorWeeklyReport($user, [$article]);
+        $this->assertCount(1, $email->getAttachments());
+
     }
 }
